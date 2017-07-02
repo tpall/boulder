d804eafa8def035338e4c34a825f182cfe0c2434
Taavi Päll <tapa741@gmail.com>
2017-07-02 17:15:55 +0300

    added list_variables function

---
+++ R/list_variables.R
@@ -0,0 +1,69 @@
+#' List available variables
+#'
+#' Lists available variables tables in TAI/National Institute for Health Development database.
+#'
+#' @param lang Language. Estonian 'et' or english 'en'. Affects table titles and variable names.
+#' @param verbose Display messages to track progress, defaults to TRUE.
+#'
+#' @return A data_frame with following columns
+#' \describe{
+#'   \item{Database}{Database name as available at TAI.}
+#'   \item{Node}{Database node name.}
+#'   \item{Name}{Variable name. This name should be used to download variable values.}
+#'   \item{Title}{Description of the variable.}
+#'   \item{Updated}{Time of the last update.}
+#' }
+#'
+#' @examples
+#' vars <- list_variables()
+#' # In RStudio, it is convinient to use View function
+#' View(vars)
+#'
+#' @export
+#'
+list_variables <- function(lang = c("et","en"), verbose = TRUE){
+
+  lang <- match.arg(lang)
+
+  # Get databases
+  if(verbose){
+    message("Getting list of available databases.")
+  }
+
+  db <- get_available_databases()
+  db_content <- dplyr::as_data_frame(db$content)
+
+  # Get nodes
+  if(verbose){
+    message("Getting list of database nodes.")
+  }
+
+  db_nodes <- dplyr::mutate(db_content, nodes = purrr::map(dbid, ~get_nodes(.x)$content)) %>%
+    tidyr::unnest(nodes, .drop = FALSE, .sep = "_")
+
+  # Fix spaces in URL
+  db_nodes <- dplyr::mutate_at(db_nodes, "nodes_id", ~purrr::map_chr(.x, stringr::str_replace_all, " ", "%20"))
+  # Fix node name
+  db_nodes <- dplyr::mutate_at(db_nodes, "nodes_text", ~purrr::map_chr(.x, stringr::str_replace_all, "\\&auml\\;", "ä"))
+
+  # Get tables at nodes
+  if(verbose){
+    message("Getting list of tables.")
+  }
+
+  db_tables <- dplyr::mutate(db_nodes, tables = purrr::map2(dbid, nodes_id, get_tables, lang = lang))
+  db_tables <- dplyr::mutate(db_tables, tables = purrr::map(tables, "content")) %>%
+    tidyr::unnest(tables, .drop = FALSE, .sep = "_")
+
+  # Filter paths with tables (leave out empty paths)
+  db_tables <- dplyr::filter(db_tables, str_detect(tables_id, "px$"))
+
+  # Format for output
+  db_tables  <- dplyr::select(db_tables, text, nodes_text, tables_text, tables_updated) %>%
+    magrittr::set_colnames(c("Database","Node", "tables_text", "Updated")) %>%
+    tidyr::separate(tables_text, into = c("Name", "Title"), sep = ": ")
+
+  return(db_tables)
+}
+
+
