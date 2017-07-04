b5a7968d3b7286429bc219f1aa493c32df4d9367
Taavi Päll <tapa741@gmail.com>
2017-07-04 20:24:59 +0300

    made function recursive

--- R/list_variables.R
+++ R/list_variables.R
@@ -55,17 +55,42 @@ list_variables <- function(lang = c("et","en"), verbose = TRUE){
     message("Getting list of tables.")
   }
 
-  db_tables <- dplyr::mutate(db_nodes, tables = purrr::map2(dbid, nodes_id, ~get_tables(dbi = .x, node = .y, lang = lang)))
-  db_tables <- dplyr::mutate(db_tables, tables = purrr::map(tables, "content")) %>%
-    tidyr::unnest(tables, .drop = FALSE, .sep = "_")
+  # Split db_nodes into max 30 rows, otherwise the server might choke
+  db_nodessplit <- split(db_nodes, c(rep(1, 30), rep(2, nrow(db_nodes) - 30)))
+
+  qfun <- ~dplyr::mutate(.x, tables = purrr::map2(dbid, nodes_id, ~get_tables(dbi = .x, node = .y, lang = lang)))
+
+  db_tables1 <- purrr::map(db_nodessplit[1], qfun)
+  db_tables2 <- purrr::map(db_nodessplit[2], qfun)
+  db_tables <- dplyr::bind_rows(db_tables1, db_tables2)
+
+  unwrap_content <- function(tab) {
+    dplyr::mutate(tab, tables = purrr::map(tables, "content")) %>%
+      tidyr::unnest(tables, .drop = FALSE, .sep = "_")
+  }
+
+  db_tables <- unwrap_content(db_tables)
 
   # Filter paths with tables (leave out empty paths)
-  db_tables <- dplyr::filter(db_tables, stringr::str_detect(tables_id, "px$"))
+  db_pxtables <- dplyr::filter(db_tables, stringr::str_detect(tables_id, "px$"))
+
+  # Paths that need deeper dig
+  db_deeptables <- dplyr::filter(db_tables, !stringr::str_detect(tables_id, "px$"))
+  db_deeptables <- dplyr::mutate(db_deeptables, nodes_id = file.path(nodes_id, tables_id)) %>%
+    dplyr::select(-dplyr::contains("tables")) %>%
+    dplyr::mutate(tables = purrr::map2(dbid, nodes_id, ~get_tables(dbi = .x, node = .y, lang = lang)))
+
+  db_deeptables <- unwrap_content(db_deeptables)
+  db_deeptables <- dplyr::filter(db_deeptables, purrr::map_lgl(tables_id, stringr::str_detect, "px$"))
+
+  # Merge results
+  db_tables <- dplyr::bind_rows(db_pxtables, db_deeptables)
 
   # Format for output
   db_tables  <- dplyr::select(db_tables, text, nodes_id, tables_text, tables_updated) %>%
     magrittr::set_colnames(c("Database","Node", "tables_text", "Updated")) %>%
-    tidyr::separate(tables_text, into = c("Name", "Title"), sep = ": ")
+    tidyr::separate(tables_text, into = c("Name", "Title"), sep = ":") %>%
+    dplyr::mutate_at(dplyr::vars(Name, Title), trimws)
 
   return(db_tables)
 }
