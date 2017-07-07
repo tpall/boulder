d0d522c92966c872f1e8e3bc240b0c0450cb9200
Taavi Päll <tapa741@gmail.com>
2017-07-07 14:42:45 +0300

    more globalvars

--- R/zzz.R
+++ R/zzz.R
@@ -2,4 +2,4 @@
 utils::globalVariables(c("dbid", "nodes", "nodes_id",
                          "tables", "str_detect", "tables_id",
                          "text", "nodes_text", "tables_text",
-                         "tables_updated", "status_code"))
+                         "tables_updated", "status_code", "Name", "Title"))
