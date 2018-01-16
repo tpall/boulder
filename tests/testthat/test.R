8ff91734458d0db56be9ce6ade356cc7192bad3a
Taavi Päll <tapa741@gmail.com>
2018-01-16 21:11:14 +0200

    tests update

--- tests/testthat/test.R
+++ tests/testthat/test.R
@@ -1,6 +1,6 @@
 
 library(boulder)
-context("API")
+context("API get functions")
 
 test_that("API produces correct structure", {
   expect_is(get_databases(), "tai_api")
@@ -22,11 +22,14 @@ test_that("API produces correct structure", {
 })
 
 
-context("Bundled data")
+context("get_all_tables")
 
 testthat::test_that("Table with data table names has correct format", {
   expect_is(vars_et, "data.frame")
   expect_is(vars_en, "data.frame")
+  expect_is(get_all_tables(), "data.frame")
+  expect_is(get_all_tables(), "tai_tables")
+  expect_is(get_all_tables(local = FALSE), "data.frame")
 })
 
 
